5ab2233beeaf6d6e1a7ae699c93d77caba282f2e
Ektorlin <hpleanos@gmail.com>
2026-08-13 15:00:47 -0600

    framework configurado

---
+++ codecept.conf.js
@@ -0,0 +1,56 @@
+require('dotenv').config({ path: './secreto.env' })
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'https://www.liverpool.com.mx',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshot: {
+      enabled: true,
+      on: 'fail'
+    },
+    allure: {
+      enabled: true,
+      require: '@codeceptjs/allure-legacy',
+      outputDir: './output/allure-results'
+    },
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {},
+    pauseOn: {},
+    browser: {},
+    aiTrace: {}
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: './tests/*_test.js',
+  noGlobals: true,
+  name: 'nuevo-framework'
+}
